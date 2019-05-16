132d56fe8aedc00a6e54b3a868cdec883cef7c89
momoko-n <mm_ayhrn@yahoo.co.jp>
2019-05-16 23:24:30 +0900

    Update _side_bar.html.haml

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  #一旦コメントアウトdevise :database_authenticatable, :registerable,
-        #一旦コメントアウト:recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable,
+        :recoverable, :rememberable, :validatable
 
   has_many :messages
   has_many :group_users
