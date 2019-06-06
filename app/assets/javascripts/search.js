803fe15676ffbb29cfe0414b1dd588ea53a0319c
momoko-n <mm_ayhrn@yahoo.co.jp>
2019-06-06 20:58:25 +0900

    Create search.js

---
+++ app/assets/javascripts/search.js
@@ -0,0 +1,12 @@
+$(document).on('turbolinks:load', function(){
+  $("#user-search-field").on("keyup", function() {
+    var input = $("#user-search-field").val();
+
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { name : input },
+      dataType: 'json'
+    })
+  });
+});
