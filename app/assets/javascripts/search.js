18704a89859be892fb5c5b51c11296edeceaec6e
momoko-n <mm_ayhrn@yahoo.co.jp>
2019-06-08 17:56:00 +0900

    minor change(Incremental-search)

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -2,18 +2,13 @@ $(document).on('turbolinks:load', function() {
   var search_list = $(".user-search-result");
 
   function appendUser(user) {
-    var html = `<div class='chat-group-user clearfix' id='chat-group-user-${user.id}'>
+    var html = `<div class='chat-group-user clearfix' >
                   <p class='chat-group-user__name'>${user.name}</p>
                     <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
                 </div>`
     search_list.append(html);
   };
-
-  // function appendNoUser(user) {
-  //   var html = ``
-  //   search_list.append(html);
-  // };
-
+  
   function buildHTML(id, name) {
     var html = `<div class="chat-group-user clearfix" id=chat-group-user-${id}>
                   <input type="hidden" name="group[user_ids][]" value="${id}">
@@ -37,17 +32,13 @@ $(document).on('turbolinks:load', function() {
         users.forEach(function(user) {
           appendUser(user);
         });
-      } else {
-        appendNoUser("一致するユーザーはいません")
-      }
+      } 
     })
     .fail(function() {
       alert('ユーザー検索に失敗しました')
     });
   });
 
-
-  //グループに追加したユーザーは表示しない
   $(".user-search-result").on('click','.user-search-add', function() {
     var id = $(this).data('user-id');
     var name = $(this).data('user-name');
